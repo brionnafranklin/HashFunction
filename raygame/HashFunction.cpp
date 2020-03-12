8682bf2a1ca590e5b35bb032cdb8c9ecd3b369e7
Brionna Franklin <54289307+brionnafranklin@users.noreply.github.com>
2020-03-12 11:09:17 -0500

    added more comments

--- raygame/HashFunction.cpp
+++ raygame/HashFunction.cpp
@@ -10,6 +10,7 @@ namespace HashFunction
 	{
 		unsigned int hash = 0;
 
+		//add the data at the index of i to hash
 		for (unsigned int i = 0; i < length; ++i)
 			hash += data[i];
 
@@ -23,9 +24,11 @@ namespace HashFunction
 
 		for (unsigned int i = 0; i < length; i++)
 		{
+			//multiplyn hash by 1313 and add data at the index of i
 			hash = (hash * 1313) + data[i];
 		}
 
+		//output as a color with alpha value
 		return (hash & 0x7FFFFFFF);
 	}
 
@@ -36,9 +39,11 @@ namespace HashFunction
 
 		for (unsigned int i = 0; i < length; i++)
 		{
+			//multiplyn hash by 1313 and add data at the index of i
 			hash = (hash * 1313) + data[i];
 		}
 
+		//return hash as a color
 		return (hash & 0xFFFFFF);
 	}
 
@@ -49,17 +54,23 @@ namespace HashFunction
 
 		for (unsigned int i = 0; i < length; i++)
 		{
+			//multiply 998877 by hash divided 2189. then subtract the data at the index of i by pi
 			hash = (998877 * (hash / 2189) - data[i]) / M_PI;
+			//if hash is more than 999999 
 			if (hash > 999999)
 			{
+				//subtract the length multiplied by hash from hash. than add 420 divide by 69
 				hash = hash - (length * hash) + 420 / 69;
 			}
+			//if the data at the index of i plus 5 is 10 or more
 			if (data[i] + 5 >= 10)
 			{
+				//hash is hash divided by 2 plus 69
 				hash = (hash / 2) + 69;
 			}
 		}
 
+		//return as color
 		return (hash & 0xFFFFFF);
 	}
 }
